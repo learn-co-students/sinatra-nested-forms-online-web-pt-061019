a432aaf646e8e518857994df75c8c8d5433bf1ea
scrabill <shannon.crabill@gmail.com>
2019-09-18 16:03:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,46 @@ module FormsLab
 
     # code other routes/actions here
 
+    get "/" do
+      erb :root
+    end
+
+    get "/new" do
+      erb :'pirates/new'
+    end
+
+    get "/pirates" do
+      erb :'pirates/show'
+
+      "Hello"
+    end
+
+    post "/pirates" do
+
+      # Create a pirate
+      @pirate = Pirate.new(params['pirate']['name'], params['pirate']['weight'], params['pirate']['height'])
+
+      # @pirate_name = params[:pirate][:name]
+      # @pirate_weight = params[:pirate][:weight]
+      # @pirate_height = params[:pirate][:height]
+
+      # Create ships
+
+      params['pirate']['ships'].each do |ship_info|
+        @ship = Ship.new(ship_info['name'],ship_info['type'], ship_info['booty'])
+      end
+
+      @ship
+
+
+      # @pirate_name = @pirate.name
+      # @pirate_weight = params[:pirate][:weight]
+      # @pirate_height = params[:pirate][:height]
+      # ["pirate", {"name"=>"Shannon Crabill", "weight"=>"123", "height"=>"213", "ships"=>[{"name"=>"bdsds", "type"=>"23", "booty"=>"12"}, {"name"=>"dfg", "type"=>"fdg", "booty"=>"fdg"}]}]
+      # params # Use this to expose all params
+
+      erb :'pirates/show'
+    end
+
   end
 end
