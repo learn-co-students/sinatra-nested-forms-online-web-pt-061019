8002a1859211bd4e386b60fda8e90149972b1bc1
Brittany Javalera <brittjavalera@gmail.com>
2019-09-19 22:59:49 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_reader :name, :height, :weight
+    PIRATES = []
+
+    def initialize(args)
+        @name =  args[:name]
+        @height = args[:height]
+        @weight = args[:weight]
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_reader :type, :name, :booty
+    SHIPS = []
+
+    def initialize(params)
+        @type = params[:type]
+        @name = params[:name]
+        @booty = params[:booty]
+        SHIPS << self
+    end
+
+    def self.all
+        SHIPS
+    end
+
+    def self.clear
+        SHIPS.clear
+    end
+
 end
